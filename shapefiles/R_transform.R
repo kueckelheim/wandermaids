9b611d85dd06824f729b240dceeb965e49d5b58b
ek2 <erisch06@aol.com>
2019-03-04 16:05:57 -0300

    worked on home map. created simplified shape files to reduce sice with R. added animation for zooming to a country and out. colored the map

---
+++ shapefiles/R_transform.R
@@ -0,0 +1,66 @@
+rm(list = ls())
+setwd("C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles")
+
+library(rgdal)
+
+
+countries <- readOGR("C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles", "ne_50m_admin_0_countries")
+
+
+# get coordinates
+b <- bbox(countries)
+
+# create a box with desired subset
+b[2,2] <- 24.5
+b[2,1] <- 5
+b[1,1] <- 95
+b[1,2] <- 130
+
+plot(countries, xlim = b[1, ], ylim = b[2, ])
+
+# clip subselection
+
+library(raster)
+library(rgeos)
+
+gClip <- function(shp, bb){
+  if(class(bb) == "matrix") b_poly <- as(extent(as.vector(t(bb))), "SpatialPolygons")
+  else b_poly <- as(extent(bb), "SpatialPolygons")
+  gIntersection(shp, b_poly, byid = T)
+}
+
+asia <- gClip(countries, b)
+
+# Extract polygon ID's
+( pid <- sapply(slot(asia, "polygons"), function(x) slot(x, "ID")) )
+# Create dataframe with correct rownames
+( p.df <- data.frame( ID=1:length(asia), row.names = pid) )  
+# add country names
+p.df$country <- NA
+p.df$country[1] <- "Vietnam"
+p.df$country[2] <- "Thailand"
+p.df$country[4] <- "Philippines"
+p.df$country[6] <- "Laos"
+p.df$country[12] <- "Cambodia"
+
+# Try coersion again and check class
+asia2 <- SpatialPolygonsDataFrame(asia, p.df)
+
+test <- asia2[which(asia2$country=="Cambodia"),]
+plot(test)
+
+writeOGR(asia2, "C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles", "asia_frame", driver="ESRI Shapefile")
+
+## Laos
+vietnam <- asia2[which(asia2$country=="Vietnam"),]
+laos <- asia2[which(asia2$country=="Laos"),]
+thailand <- asia2[which(asia2$country=="Thailand"),]
+philippines <- asia2[which(asia2$country=="Philippines"),]
+cambodia <- asia2[which(asia2$country=="Cambodia"),]
+
+
+writeOGR(vietnam, "C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles", "vietnam", driver="ESRI Shapefile")
+writeOGR(laos, "C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles", "laos", driver="ESRI Shapefile")
+writeOGR(thailand, "C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles", "thailand", driver="ESRI Shapefile")
+writeOGR(philippines, "C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles", "philippines", driver="ESRI Shapefile")
+writeOGR(cambodia, "C:\\Users\\EK\\Documents\\web\\react\\blog\\shapefiles", "cambodia", driver="ESRI Shapefile")
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import "./home.scss";
 
 import {
   ComposableMap,
@@ -11,78 +12,333 @@ import {
   Markers,
   Marker
 } from "react-simple-maps";
+import { Motion, spring } from "react-motion";
 
 class Home extends Component {
   static propTypes = {
     blogs: PropTypes.array.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: [112.5, 14.75],
+      zoom: 1,
+      countries: [
+        { name: "Thailand", coordinates: [98.4684384, 11.7996671] },
+        { name: "Philippines", coordinates: [122.095, 12.26552] },
+        { name: "Vietnam", coordinates: [106.597532, 15.911419] },
+        { name: "Cambodia", coordinates: [105.393584, 12.686335] },
+        { name: "Laos", coordinates: [103.575512, 17.465869] }
+      ]
+    };
+  }
+
+  handleCountry = evt => {
+    console.log(evt.target.value);
+    const select = evt.target.value;
+    const select_object = this.state.countries[select];
+    this.setState({
+      center: select_object.coordinates,
+      zoom: 1
+    });
+  };
+  handleReset = () => {
+    this.setState({
+      center: [112.5, 14.75],
+      zoom: 1
+    });
+  };
+
   render() {
     return (
       <div className="Home">
-        <div className="map">
-          <ComposableMap
-            projectionConfig={{
-              scale: 205,
-              rotation: [-11, 0, 0]
-            }}
-            width={980}
-            height={551}
-            style={{
-              width: "100%",
-              height: "auto"
-            }}
-          >
-            <ZoomableGroup
-              center={[111.8781121, 14.4266586]}
-              zoom="8"
-              disablePanning
+        <div className="mapWrapper">
+          <div className="map">
+            <Motion
+              defaultStyle={{
+                zoom: 1,
+                x: 112.5,
+                y: 14.75
+              }}
+              style={{
+                zoom: spring(this.state.zoom, { stiffness: 210, damping: 20 }),
+                x: spring(this.state.center[0], {
+                  stiffness: 210,
+                  damping: 20
+                }),
+                y: spring(this.state.center[1], { stiffness: 210, damping: 20 })
+              }}
             >
-              <Geographies geography={require("../../world-50m.json")}>
-                {(geographies, projection) =>
-                  geographies.map(
-                    (geography, i) =>
-                      geography.id !== "ATA" && (
-                        <Geography
-                          key={i}
-                          geography={geography}
-                          projection={projection}
-                          style={{
-                            default: {
-                              fill: "#ECEFF1",
-                              stroke: "#607D8B",
-                              strokeWidth: 0.15,
-                              outline: "none"
-                            },
-                            hover: {
-                              fill: "#607D8B",
-                              stroke: "#607D8B",
-                              strokeWidth: 0.75,
-                              outline: "none"
-                            },
-                            pressed: {
-                              fill: "#FF5722",
-                              stroke: "#607D8B",
-                              strokeWidth: 0.75,
-                              outline: "none"
-                            }
-                          }}
-                        />
-                      )
-                  )
-                }
-              </Geographies>
-            </ZoomableGroup>
-          </ComposableMap>
-        </div>
-        {this.props.blogs.map(blog => (
-          <div key={blog.title}>
-            <Link to={"/" + blog.title}>
-              <h3>{blog.title}</h3>
-            </Link>
-            <p>{blog.short_description}</p>
+              {({ zoom, x, y }) => (
+                <ComposableMap
+                  projectionConfig={{
+                    scale: 2005,
+                    rotation: [-11, 0, 0]
+                  }}
+                  width={820}
+                  height={551}
+                  className="mapFrame"
+                >
+                  <ZoomableGroup center={[x, y]} zoom={zoom} disablePanning>
+                    <Geographies geography={require("../../maps/map.json")}>
+                      {(geographies, projection) =>
+                        geographies.map(
+                          (geography, i) =>
+                            geography.id !== "ATA" && (
+                              <Geography
+                                key={i}
+                                geography={geography}
+                                projection={projection}
+                                style={{
+                                  default: {
+                                    fill: "#e6e6e6",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.45,
+                                    outline: "none"
+                                  },
+                                  hover: {
+                                    fill: "#e6e6e6",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.45,
+                                    outline: "none"
+                                  },
+                                  pressed: {
+                                    fill: "#e6e6e6",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.45,
+                                    outline: "none"
+                                  }
+                                }}
+                              />
+                            )
+                        )
+                      }
+                    </Geographies>
+                    <Geographies
+                      geography={require("../../maps/thailand.json")}
+                    >
+                      {(geographies, projection) =>
+                        geographies.map(
+                          (geography, i) =>
+                            geography.id !== "ATA" && (
+                              <Geography
+                                key={i}
+                                geography={geography}
+                                projection={projection}
+                                style={{
+                                  default: {
+                                    fill: "#f7eb89",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  hover: {
+                                    fill: "#f7eb89",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  pressed: {
+                                    fill: "#f7eb89",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  }
+                                }}
+                              />
+                            )
+                        )
+                      }
+                    </Geographies>
+                    <Geographies
+                      geography={require("../../maps/cambodia.json")}
+                    >
+                      {(geographies, projection) =>
+                        geographies.map(
+                          (geography, i) =>
+                            geography.id !== "ATA" && (
+                              <Geography
+                                key={i}
+                                geography={geography}
+                                projection={projection}
+                                style={{
+                                  default: {
+                                    fill: "#f3d8c5",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  hover: {
+                                    fill: "#f3d8c5",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  pressed: {
+                                    fill: "#f3d8c5",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  }
+                                }}
+                              />
+                            )
+                        )
+                      }
+                    </Geographies>
+                    <Geographies geography={require("../../maps/laos.json")}>
+                      {(geographies, projection) =>
+                        geographies.map(
+                          (geography, i) =>
+                            geography.id !== "ATA" && (
+                              <Geography
+                                key={i}
+                                geography={geography}
+                                projection={projection}
+                                style={{
+                                  default: {
+                                    fill: "#bbd8b9",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  hover: {
+                                    fill: "#bbd8b9",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  pressed: {
+                                    fill: "#bbd8b9",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  }
+                                }}
+                              />
+                            )
+                        )
+                      }
+                    </Geographies>
+                    <Geographies
+                      geography={require("../../maps/philippines.json")}
+                    >
+                      {(geographies, projection) =>
+                        geographies.map(
+                          (geography, i) =>
+                            geography.id !== "ATA" && (
+                              <Geography
+                                key={i}
+                                geography={geography}
+                                projection={projection}
+                                style={{
+                                  default: {
+                                    fill: "#c3abd4",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  hover: {
+                                    fill: "#c3abd4",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  pressed: {
+                                    fill: "#c3abd4",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  }
+                                }}
+                              />
+                            )
+                        )
+                      }
+                    </Geographies>
+                    <Geographies geography={require("../../maps/vietnam.json")}>
+                      {(geographies, projection) =>
+                        geographies.map(
+                          (geography, i) =>
+                            geography.id !== "ATA" && (
+                              <Geography
+                                key={i}
+                                geography={geography}
+                                projection={projection}
+                                style={{
+                                  default: {
+                                    fill: "#e19b93",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  hover: {
+                                    fill: "#e19b93",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  },
+                                  pressed: {
+                                    fill: "#e19b93",
+                                    stroke: "#607D8B",
+                                    strokeWidth: 0.65,
+                                    outline: "none"
+                                  }
+                                }}
+                              />
+                            )
+                        )
+                      }
+                    </Geographies>
+                  </ZoomableGroup>
+                </ComposableMap>
+              )}
+            </Motion>
           </div>
-        ))}
+          <div className="countryList">
+            <ul>
+              <li>
+                <button onClick={this.handleCountry} value="0">
+                  Thailand
+                </button>
+              </li>
+              <li>
+                <button onClick={this.handleCountry} value="1">
+                  Philippines
+                </button>
+              </li>
+              <li>
+                <button onClick={this.handleCountry} value="2">
+                  Vietnam
+                </button>
+              </li>
+              <li>
+                <button onClick={this.handleCountry} value="3">
+                  Cambodia
+                </button>
+              </li>
+              <li>
+                <button onClick={this.handleCountry} value="4">
+                  Laos
+                </button>
+              </li>
+              <li>
+                <button onClick={this.handleReset}>Reset</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <main>
+          {this.props.blogs.map(blog => (
+            <div key={blog.title}>
+              <Link to={"/" + blog.title}>
+                <h3>{blog.title}</h3>
+              </Link>
+              <p>{blog.short_description}</p>
+            </div>
+          ))}
+        </main>
       </div>
     );
   }
